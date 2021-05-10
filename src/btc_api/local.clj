31115b5357272d95bcdf7a6b56210a7e3f501226
Anders Eliasson <anders.eliasson@digitalroute.com>
2021-05-10 21:33:24 +0200

    added more fns to core

--- src/btc_api/local.clj
+++ src/btc_api/local.clj
@@ -3,15 +3,18 @@
  (:use [clojure.pprint])
  (:require [clojure.data.json :as json]
            [clojure.java.shell :refer [sh]]
-           [btc-api.core :refer :all]))
+           #_[btc-api.core :refer :all]))
+
+(def bitcoin-cli "/Users/anderseliasson/src/bitcoin/src/bitcoin-cli")
+
 (defn cli [& args]
-  (let [res (apply sh (concat ["/Users/anderseliasson/src/bitcoin/src/bitcoin-cli"] (map str args)))]
+  (let [res (apply sh (concat [bitcoin-cli] (map str args)))]
     (if (= (:exit res) 0)
       (apply str (butlast (:out res)))
       (throw (IllegalStateException. (:err res))))))
 
 
-(defmacro def-cli [cmd & args] 
+(defmacro def-cli-json [cmd & args] 
   `(defn ~cmd [~@args] 
      (let [res# (cli ~(str cmd) ~@args)]
        (try 
@@ -19,51 +22,51 @@
          (catch Exception e#
            res#)))))
 
-#_(defmacro def-json-cli [cmd & args]
-   `(defn ~cmd [~@args]
-      (let [cli-fn# (partial cli ~(str cmd) "-f" "json")]
-          (json/read-json (cli-fn# ~@args)))))
+(defmacro def-cli [cmd & args] 
+  `(defn ~cmd [~@args] 
+     (cli ~(str cmd) ~@args)))
 
-
-
-
-(deftype BtcCore []
-    Node
-    (current-height [_] (read-string (cli "getblockcount")))
-    (block-hash-of [_ height] (cli "getblockhash" (str height)))
-    (block-header-of [node height-or-hash] 
-      (json/read-json 
-        (cli "getblockheader" 
-             (if (number? height-or-hash) 
-               (block-hash-of node height-or-hash) 
-               height-or-hash))))
-    (block-of [node height-or-hash]
-      (json/read-json 
-        (cli "getblock" 
-             (if (number? height-or-hash) 
-               (block-hash-of node height-or-hash) 
-               height-or-hash))))
-    (tx-of [node tx-hash]
-      (json/read-json (cli "decoderawtransaction" (cli "getrawtransaction" tx-hash))))
-)
+(def-cli getrawtransaction tx-id) 
+(def-cli createrawtransaction in out)
+(def-cli-json decoderawtransaction raw-tx)
+(defn createmultisig [m pub-keys]
+  (json/read-str (cli "createmultisig" (str m) (json/json-str pub-keys))))
+(defn signrawtransactionwithkey [tx-in-hex private-keys]
+  (json/read-str (cli "signrawtransactionwithkey" tx-in-hex (json/json-str private-keys))))
 
 ;blockchain
 (def-cli getblockcount)
 (def-cli getblockhash height)
+(def-cli-json getblockheader block-hash)
+(def-cli-json getblock block-hash)
+
+(defn block-header-of [height-or-hash]
+    (getblockheader 
+      (if (number? height-or-hash) 
+        (getblockhash height-or-hash) 
+        height-or-hash)))
+
+(defn block-of [height-or-hash]
+         (getblock 
+           (if (number? height-or-hash) 
+             (getblockhash height-or-hash) 
+             height-or-hash)))
+
+(defn tx-of [tx-hash]
+  (decoderawtransaction (getrawtransaction tx-hash)))
 
 
-(defn createmultisig [m pub-keys]
-  (json/read-str (cli "createmultisig" (str m) (json/json-str pub-keys))))
-
-(def-cli getrawtransaction tx-id) 
-(def-cli createrawtransaction in out)
 
 ;wallet
 (def-cli getnewaddress)
-(def-cli sendtoaddress address btc)
-(def-cli getbalance)
+(defn sendtoaddress! [address btc] (cli "sendtoaddress" address (str btc)))
+(def-cli-json getbalance)
 (def-cli listwallets)
+(defn addmultisigaddress! [m hex-or-address-pub-key] 
+  (json/read-str (cli "addmultisigaddress" m (json/json-str hex-or-address-pub-key))))
 
+
+;examples
 
 (def key-1 {:private "5KffUB9YUsvoGjrcn76PjVnC61PcWLzws4QPfrT9RFNd85utCkZ"
             :public "04A97B658C114D77DC5F71736AB78FBE408CE632ED1478D7EAA106EEF67C55D58A91C6449DE4858FAF11721E85FE09EC850C6578432EB4BE9A69C76232AC593C3B"})
@@ -74,9 +77,12 @@
 
 (def multisig (createmultisig 2 (map :public [key-1 key-2 key-3])))
 
+(def pay-to-address "tb1qxpqaddgec3thrkl2s5za7zv6gzy9d23whl26aw")
 (comment
-  (sendtoaddress (multisig "address") (/ (getbalance)  10))
+  (sendtoaddress! (multisig "address") (/ (getbalance)  10))
   (def tx-id "a88f4aaface0bfd90e9f1109db3449a67f090fa9051296ce22842babb8fb9ede")
+  (tx-of tx-id)
+  (def pay-tx (createrawtransaction (json/json-str [{:txid tx-id, "vout" 1}]) (json/json-str [{pay-to-address 0.00009}])))
   )
   
 
--- test/btc_api/core_test.clj
+++ test/btc_api/core_test.clj
@@ -1,7 +1,11 @@
 (ns btc-api.core-test
   (:require [clojure.test :refer :all]
+            [base58.core :as base58]
             [btc-api.core :refer :all]))
 
 
 
-(
+(deftest test-base58
+  (is (= "2NEpo7TZRRrLZSi2U" (base58/encode "Hello World!" )))
+  (is (= "Hello World!" (base58/decode "2NEpo7TZRRrLZSi2U" )))
+  )
